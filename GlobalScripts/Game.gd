63a7450c7b4eddbdabdc418f88374bc425deb144
Flofzter <25ghu@go.dsdmail.net>
2023-11-15 21:19:54 -0700

    Implemented save system
    The basis of the save system has been implemented.

--- GlobalScripts/Game.gd
+++ GlobalScripts/Game.gd
@@ -2,5 +2,6 @@ extends Node
 
 var playerHP = 100
 var playerMaxHP = 100
+var money = 0
 
 var room = 0
--- Scenes/Main/MainScene.gd
+++ Scenes/Main/MainScene.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 "Start Game area"
 func _on_start_player_detection_body_entered(body:Node2D):
 	if body.name == "Player":
-		get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+		if Utils.saveExists():
+			print("Save file found!\nLOADING SAVE FILE")
+			Utils.loadGame()
+			get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+		else:
+			get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
