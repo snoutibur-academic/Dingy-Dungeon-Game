f03ea6e8d1ec9bc3b61601210473f73c954aa044
Flofzter <25ghu@go.dsdmail.net>
2023-11-19 13:02:09 -0700

    Enemy chases player. Load options screen implemented.

--- Scenes/Main/MainScene.gd
+++ Scenes/Main/MainScene.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
 func _ready():
-	Utils.loadGame()
+	pass
 
-"Start Game area"
+"Start Game Area"
 func _on_start_player_detection_body_entered(body:Node2D):
 	if body.name == "Player":
-		if Utils.saveExists():
-			print("Save file found!\nLOADING SAVE FILE")
-		
-		get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+		if Utils.saveExists(): # If player has an existing save file, ask if they wish to load it
+			$LoadSave.show()
+		else: 
+			get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+
+
+"Load save menu"
+func _on_no_load_button_pressed():
+	get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+
+func _on_load_button_pressed():
+	Utils.loadGame()
+	get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
