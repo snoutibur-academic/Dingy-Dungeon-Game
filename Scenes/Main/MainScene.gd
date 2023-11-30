c8e3763b046ae7fe9e061b598f0360f9aac05f2a
Flofzter <25ghu@go.dsdmail.net>
2023-11-30 11:00:08 -0700

    added sword!!
    IT WORKSS!!!

--- Scenes/Main/MainScene.gd
+++ Scenes/Main/MainScene.gd
@@ -9,7 +9,7 @@ func _on_start_player_detection_body_entered(body:Node2D):
 		if Utils.loadGame(): # If player has an existing save file, ask if they wish to load it
 			$LoadSave.show()
 		else: 
-			get_tree().change_scene_to_file(("res:///Scenes/Levels/1Level.tscn"))
+			get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
 
 
 "Load save menu"
