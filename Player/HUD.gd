046153726499968d7df17a67523007f904bf95ce
snoutibur <157987813+snoutibur@users.noreply.github.com>
2023-12-01 10:07:28 -0700

    Upgrade menu

--- Player/HUD.gd
+++ Player/HUD.gd
@@ -1,9 +1,21 @@
 extends CanvasLayer
 
-func _ready():
-	pass # Replace with function body.
+@onready var  UpgradeMenu = $UpgradeMenu
 
 func _process(delta):
 	"Label Updates"
 	$HealthLabel.text = "HP: " + str(Game.playerHP) + " / " + str(Game.playerMaxHP)
 	$RoomNumberLbl.text = str(Game.room)
+
+"Upgrades"
+func _on_upgrade_button_pressed():
+	UpgradeMenu.visible = true
+func _on_close_menu_pressed():
+	UpgradeMenu.visible = false
+
+func _on_upgrade_attack_button_pressed():
+	Game.plrAtkDmg += 3
+
+func _on_button_pressed():
+	Utils.saveGame()
+
