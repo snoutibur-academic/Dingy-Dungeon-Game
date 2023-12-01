3d87205961a673955a5ae896e8fc4f0e73a39b2e
snoutibur <157987813+snoutibur@users.noreply.github.com>
2023-12-01 16:46:17 -0700

    Finalize (hopefully) upgrades

--- Scenes/Main/MainScene.gd
+++ Scenes/Main/MainScene.gd
@@ -15,4 +15,4 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file(("res:///Scenes/Levels/1Level.tscn"))
 func _on_start_from_save_pressed():
 	Utils.loadGame()
-	get_tree().change_scene_to_file(("res:///Scenes/Levels/DebugLevel.tscn"))
+	get_tree().change_scene_to_file(("res:///Scenes/Levels/1Level.tscn"))
